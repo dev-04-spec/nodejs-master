b172e1a42397e5f5b3cbc9e6d253e8572e8e79c1
manojk08 <kalemanoj3@gmail.com>
2020-08-12 00:21:07 +0530

    Calling Endpoints using postman

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express=require('express');
 const app= express();
 
-app.get('/',(req, res) => {
-    res.send('Hello World');
+app.use(express.json());
+const courses=[
+    {id:1,name:'Angular'},
+    {id:2,name:'React JS'},
+    {id:3,name:'Node JS'}
+]
+
+app.get('/api/courses',(req, res) => {
+    res.send(courses);
+});
+app.post('/api/courses',(req, res)=>{
+    const course={
+        id:courses.length+1, 
+        name:req.body.name
+    }
+    courses.push(course);
+    res.send(courses);
 })
-app.get('/api/cources',(req, res) => {
-    res.send([1,2,3]);
+app.get('/api/courses/:id',(req, res) => {
+    const course=courses.find(c=>{
+        return c.id===parseInt(req.params.id)
+    })
+    if(!course){
+        res.status(404).send("The course with the given id was not found");
+
+    }
+    res.send(course);
+    
 });
 
 
-//PORT
+
 
 const port =process.env.PORT ||3000
 app.listen(port,()=>console.log(`Listening on port ${port}`));
